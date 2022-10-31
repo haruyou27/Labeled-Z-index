586e159433e3c618e28ab97e08fc861977901112
Haruyou27 <emaildai27@gmail.com>
2022-10-31 23:05:35 +0700

    fix file path

--- InspectorInterface.gd
+++ InspectorInterface.gd
@@ -1,13 +1,13 @@
 extends EditorInspectorPlugin
 
-var p_interface := preload("res://addons/BetterZindex/PropertyInterface.gd")
+var p_interface := preload("res://addons/Labeled-Z-index/PropertyInterface.gd")
 
 func can_handle(object) -> bool:
 	return object is CanvasItem
 
 func parse_category(object, category):
 	if category != 'CanvasItem':
-		return false
+		return
 	
 	var current := -1
 	if object is Node2D:
@@ -15,4 +15,3 @@ func parse_category(object, category):
 		current = ProjectSettings.get('layer_names/z_index/z_index').values().find(object.z_index)
 	
 	add_property_editor(category, p_interface.new(current))
-	return true
